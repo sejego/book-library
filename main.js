8528ddbe8edf2d516aa5345a6df8905dd237cac4
sejego <sejego@taltech.ee>
2022-03-14 00:59:47 +0200

    add localStorage support, update style, add checkbox in the form and cancel button

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-bookLibrary = new Library();
-
-let bookForm = document.querySelector("form");
-bookForm.style.visibility = "hidden";
-const addNewBtn = document.getElementsByTagName("button")[0];
-addNewBtn.addEventListener('click',showTextFields);
-
+/* Library class to hold Book objects, remove them and render them in DOM */
 
 function Library() {
     this.idx = 0;
@@ -14,9 +8,13 @@ function Library() {
 
 Library.prototype.push = function(book) {
     this.books.push(book);
+    if(canStore) {
+        localStorage.setItem("books", JSON.stringify(this.books));
+    }
     this.renderBooks();
-    hideTextFields();
 }
+
+/* Remove a book and update the DOM */
 
 Library.prototype.removeBook = function(book) {
     let bookIdx = 0;
@@ -26,8 +24,13 @@ Library.prototype.removeBook = function(book) {
             break;
         }
     }
+    if(canStore) {
+        localStorage.setItem("books", JSON.stringify(this.books));
+    }
     this.renderBooks();
 }
+
+/* Remove the existing books and render an updated list of books */
 
 Library.prototype.renderBooks = function() {
 
@@ -42,6 +45,8 @@ Library.prototype.renderBooks = function() {
         this.renderABook(this.books[i], i);
     }
 }
+
+/* Render one book by adding it as a row in a table of books */
 
 Library.prototype.renderABook = function(book, idx) {
 
@@ -80,6 +85,8 @@ Library.prototype.renderABook = function(book, idx) {
     row.cells[6].appendChild(removeBtn);
 }
 
+/* Two methods for creating change and remove buttons next to every book. */
+
 Library.prototype.createRemoveBtn = function() {
     const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Remove';
@@ -87,10 +94,12 @@ Library.prototype.createRemoveBtn = function() {
 }
 
 Library.prototype.createChangeBtn = function() {
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Change';
-    return removeBtn;
+    const changeBtn = document.createElement('button');
+    changeBtn.textContent = 'Change Status';
+    return changeBtn;
 }
+
+/* Book class holding all the information about books. */
 
 function Book(title = "Untitled", genre = "Unknown", year = "Unknown", author = "Unknown") {
     this.title = title;
@@ -105,6 +114,9 @@ Book.prototype.changeStatus = function() {
     this.isRead = !this.isRead;
 }
 
+/* Global function that is executed by pressing the Add button.
+Creates a book, pushed it into the Library and hides the form */
+
 function submitNewBook(form) {
 
     let book = new Book();
@@ -112,23 +124,66 @@ function submitNewBook(form) {
     book.author = form.author.value;
     book.year = form.year.value;
     book.genre = form.genre.value;
+    book.isRead = form.checkbox.checked;
     bookLibrary.push(book);
+    hideFormFields();
 }
 
-function showTextFields() {
+function showFormFields() {
     // shows new form for adding books
     form = document.querySelector("form");
     form.style.visibility = "visible";
 }
 
-function hideTextFields(form) {
+function hideFormFields(form) {
     // clears and hides the form for adding books
     form = document.querySelector("form");
     form.reset();
     form.style.visibility = "hidden";
-
 }
 
-function isEmpty(str) {
-    return !str.trim().length;
+/* Taken from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API */
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
 }
+
+let bookLibrary = new Library(); 
+let canStore = false;
+
+
+/* If local storage is available, it will set global variable canStore to allow saving books to local storage
+and add all existing books in the storage to Library. every parsed object must be assigned a Book prototype 
+to enable assigned methods */
+if(storageAvailable('localStorage')) {
+    canStore = true;
+    let books = JSON.parse(localStorage.getItem("books") || "[]");
+    console.log(books)
+    for(let i = 0; i < books.length; i++) {
+        bookLibrary.push(Object.assign(new Book, books[i]));
+    }
+}
+
+console.log(bookLibrary);
+let bookForm = document.querySelector("form");
+bookForm.style.visibility = "hidden";
